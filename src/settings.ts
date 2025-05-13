8f3344cede5a1e6d38287cfd81ca5a730fbf4f23
Ping.J <physcircus@PingJs-MacBook-Air.local>
2025-05-13 21:22:47 +0800

    revise theme

--- src/settings.ts
+++ src/settings.ts
@@ -24,7 +24,7 @@ export const template = {
 	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
-	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
+	lightTheme: 'wireframe', // Select one of the Daisy UI Themes or create your own
 	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
